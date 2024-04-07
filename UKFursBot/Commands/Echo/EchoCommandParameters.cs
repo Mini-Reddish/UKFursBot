791c1ca5f8b312006c035a59a91de8a74335d37d
Mini <minireddish@msn.com>
2024-04-07 21:05:54 +0100

    remmoved echo command.

--- UKFursBot/Commands/Echo/EchoCommandParameters.cs
+++
@@ -1,9 +0,0 @@
-﻿using UKFursBot.Commands.CommandClassAttributes;
-
-namespace UKFursBot.Commands.Echo;
-
-public class EchoCommandParameters
-{
-    [CommandParameterDescription("This returns the string you pass into the command")]
-    public string EchoResponseString { get; init; }
-}
